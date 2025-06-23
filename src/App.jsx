04a50f5c58244777f17679ec96dea8bf8abfd68f
Siddhant Singh <siddhantsingh19326@gmail.com>
2025-06-23 15:09:23 +0530

    created index.css and general layers components for css and added assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+        <div>
+            <h1 className=" font-bold">Welcome to my portfolio</h1>
+        </div>
+        </main>
+    )
+}
+export default App
